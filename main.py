e897435ab84aff10dc6775986882013542c233b0
johanpina <johanpina1420@gmail.com>
2023-03-17 16:46:29 -0500

    realizamos el login, modificamos las rutas del API router y terminamos de pulir algunos mértodos

--- main.py
+++ main.py
@@ -1,70 +1,29 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI,APIRouter
+from eschemas import User
+from database import USUARIOS
+from routers.usuario import user
 
 app = FastAPI()
 
+app.include_router(user,prefix='/user')
 
 @app.get("/")
 def hello_world():
     return {"message":"Servidor ejecutandose"}
 
-usuarios = [
-    {'id':1,
-    'username':'reinel',
-     'password':'quiz'
-     },
-     {'id':2,
-    'username':'james',
-     'password':'jajaja'
-     },
-     {'id':3,
-    'username':'johan',
-     'password':'pina'
-     },
-     {'id':4,
-    'username':'camilo',
-     'password':'1234'
-     },
-     {'id':5,
-    'username':'chatgpt',
-     'password':'version4'
-     }
-]
 
-@app.get("/user/all/")
-def listar_allUsers():
-    return usuarios
-
-@app.get("/user/unique/{id_usuario}")
-def listar_justOne(id_usuario:int):
-    # buscar el usuario en la lista
-
-    for user in usuarios:
-        if user['id'] == id_usuario:
-            return user
-    return {"message":"No se encontró el usuario en la DB"}
-
-
-class User(BaseModel):
-    username:str
-    id:int
-    password:str
-
-
-@app.post("/user/addusr/")
+@app.post("/diagnostico/adddiag/")
 def add_usr(user:User):
     #id
     #username
     #passwd
-    for usuario in usuarios:
+    for usuario in USUARIOS:
         if user.username == usuario['username']:
             return {"message":"Usuario ya existe"}
     
     # metodo para agregar en la base de datos
-    usuarios.append(user)
+    USUARIOS.append(user)
     return {"message": "usuario añadido con éxito"}
-
-
 
 
 
---
+++ routers/usuario.py
@@ -0,0 +1,46 @@
+
+import sys
+sys.path.append('../')
+
+from fastapi import APIRouter
+from database import USUARIOS
+from eschemas import User
+
+user = APIRouter()
+
+
+@user.get("/all/")
+def listar_allUsers():
+    return USUARIOS
+
+@user.get("/unique/{id_usuario}")
+def listar_justOne(id_usuario:int):
+    # buscar el usuario en la lista
+
+    for user in USUARIOS:
+        if user['id'] == id_usuario:
+            return user
+    return {"message":"No se encontró el usuario en la DB"}
+
+@user.post("/addusr/")
+def add_usr(user:User):
+    #id
+    #username
+    #passwd
+    for usuario in USUARIOS:
+        if user.username == usuario['username']:
+            return {"message":"Usuario ya existe"}
+    
+    # metodo para agregar en la base de datos
+    USUARIOS.append(user)
+    return {"message": "usuario añadido con éxito"}
+
+
+@user.post("/login/")
+def login(user:User):
+    for usuario in USUARIOS:
+        if (usuario["username"] == user.username) and (usuario["password"] == user.password):
+            return {"message":"Acceso correcto",
+                    "token": "ADIDCXCGT"}
+    return {"message": "Verifique su usuario y contraseña"}
+
