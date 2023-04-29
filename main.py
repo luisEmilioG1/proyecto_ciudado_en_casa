d02c720ac081802760b4ecc00ff44995b1821b4f
Juan Velasquez <juan.1701812756@ucaldas.edu.co>
2023-04-29 00:15:43 -0500

    se crea la ruta para la consulta de signos vitales en el main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from rutas.diagnostico import diagnostico
+from rutas.signo_vital import signo_vital
 
 app = FastAPI()
 
 app.include_router(diagnostico,prefix='/diagnostico')
+app.include_router(signo_vital,prefix='/signo_vital')
 
